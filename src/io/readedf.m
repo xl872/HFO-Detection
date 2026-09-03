1a5a992bcd6bbc4e119da72442734574dddde138
Xiaochen Liu <153525728+xl872@users.noreply.github.com>
2026-09-03 12:20:57 -0400

    Add EDF readers (src/io/)

---
+++ src/io/readedf.m
@@ -0,0 +1,40 @@
+
+function [data,header] = readedf(filename)
+
+if nargin < 1
+    help readedf;
+    return;
+end;
+    
+fp = fopen(filename,'r','ieee-le');
+if fp == -1
+  error('File not found ...!');
+  return;
+end
+
+hdr = setstr(fread(fp,256,'uchar')');
+header.length = str2num(hdr(185:192));
+header.records = str2num(hdr(237:244));
+header.duration = str2num(hdr(245:252));
+header.channels = str2num(hdr(253:256));
+header.channelname = setstr(fread(fp,[16,header.channels],'char')');
+header.transducer = setstr(fread(fp,[80,header.channels],'char')');
+header.physdime = setstr(fread(fp,[8,header.channels],'char')');
+header.physmin = str2num(setstr(fread(fp,[8,header.channels],'char')'));
+header.physmax = str2num(setstr(fread(fp,[8,header.channels],'char')'));
+header.digimin = str2num(setstr(fread(fp,[8,header.channels],'char')'));
+header.digimax = str2num(setstr(fread(fp,[8,header.channels],'char')'));
+header.prefilt = setstr(fread(fp,[80,header.channels],'char')');
+header.frequency = str2num(setstr(fread(fp,[8,header.channels],'char')'));
+
+fseek(fp,header.length,-1);
+data = fread(fp,'int16');
+fclose(fp);
+
+data = reshape(data,header.duration*header.frequency(1),header.channels,header.records);
+temp = [];
+for i=1:header.records
+  temp = [temp data(:,:,i)'];
+end
+data = temp;
+end
